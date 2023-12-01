f085968348a8c3c62bc8bc6620f9958d58d4b2b1
StephanieJAA <132408540+StephanieJAA@users.noreply.github.com>
2023-12-01 12:58:47 -0500

    Update BasedeDatos.php

--- Codigo-Semestral/BasedeDatos.php
+++ Codigo-Semestral/BasedeDatos.php
@@ -1,9 +1,3 @@
 <?php
 $connection = mysqli_connect('localhost', 'root', '', 'semestralweb');
-if ($connection) { 
-    echo "Conexión a la Base de Datos exitosa"; 
-} 
-else { 
-    die ("Conexion a la Base de Datos ha fallado"); 
-}
 ?>
